d359b68be9c8b50c61b953764bc25c7fd3a40430
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-04-09 15:30:08 +0300

    add service calling on useEffect

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [value, setValue] = useState(1);
+  const [visible, setVisible] = useState(true);
+
+  if (visible) {
+    return (
+      <div>
+        <button onClick={() => setValue((v) => v + 1)}>+</button>
+        <button onClick={() => setVisible(false)}>hide</button>
+        <PlanetInfo id={value} />
+      </div>
+    );
+  } else {
+    return (
+      <button
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
+        show
+      </button>
+    );
+  }
+};
+
+const PlanetInfo = ({ id }) => {
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    let cancelled = false;
+
+    fetch(`https://swapi.dev/api/planets/${id}`)
+      .then((res) => res.json())
+      .then((data) => !cancelled && setName(data.name));
+
+    return () => (cancelled = true);
+  }, [id]);
+
+  return (
+    <div>
+      {id} - {name}
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
